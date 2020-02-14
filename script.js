8c3e74e84c59fd3ae489f162f2ada9117bba80bd
agrekkedal <agrekkedal1@gmail.com>
2020-02-14 11:46:00 +0100

    test

--- script.js
+++ script.js
@@ -6,12 +6,13 @@ function getInput(){
 
 function callJson(){
     var anagram = getInput();
+    var anagram2 = anagram.split('').sort().join('');
     fetch("all_English_Words.json")
         .then(function(resp) {
             return resp.json();
         })
         .then(function(data){
-            var words = data[anagram];
+            var words = data[anagram2];
             var wordOutput = "";
             for(let i = 0; i < words.length; i++){
                 console.log(words[i]);
