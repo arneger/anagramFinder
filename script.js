e1629bee1ff8ad9fa5c7efb41ffb77f1fc5d105b
agrekkedal <agrekkedal1@gmail.com>
2020-02-14 11:24:29 +0100

    created call function

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-fetch("all_English_Names.json")
-    .then(function(resp) {
-        return resp.json();
-    })
-    .then(function(data){
-        var words = data["abel"]
-        for(let i = 0; i < words.length; i++){
-            console.log(words[i]);
-        }
-    })
+function getInput(){
+    var input2 = document.getElementById("anagramInput").value;
+    document.getElementById("anagramInput").value = '';
+    return input2;
+
+function callJson(){
+    var anagram = getInput();
+    fetch("all_English_Names.json")
+        .then(function(resp) {
+            return resp.json();
+        })
+        .then(function(data){
+            var words = data[anagram];
+            var wordOutput = "";
+            for(let i = 0; i < words.length; i++){
+                console.log(words[i]);
+                wordOutput += words[i] + "<br>";
+            }
+            document.getElementById("wordMatches").innerHTML = wordOutput;
+        })
+    }
